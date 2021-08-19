91a81b2a8b3388994ede867c1f558bea009f2e91
Shunya Suzuki <syunya_0617@outlook.jp>
2021-08-19 18:19:18 +0900

    feature: add GET tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use App\Models\Achievement;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
+use Illuminate\Support\Facades\DB; 
 
 class TaskController extends Controller
 {
-    //
+    public function getTasks(Request $request){
+        $SQL = <<< SQL
+        SELECT tasks.id, task_name, description, genre_name, difficulty_name, 
+            CASE WHEN user_id IS NULL THEN 0 ELSE 1 END AS is_achieve 
+        FROM tasks 
+        LEFT JOIN ( 
+            SELECT * FROM achievements WHERE user_id = 10
+        ) AS A ON tasks.id = A.task_id 
+        JOIN genres ON genres.id = tasks.genre_id 
+        JOIN difficulties ON difficulties.id = tasks.difficulty_id
+        SQL;
+        
+        $tasks = DB::select($SQL);
+        return $tasks;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
+Route::middleware('auth:sanctum')->get('/tasks', [TaskController::class, 'getTasks']);
