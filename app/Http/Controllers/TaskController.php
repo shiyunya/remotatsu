570f7db9e24222072c53afb2c7db007a3473aeae
Shunya Suzuki <syunya_0617@outlook.jp>
2021-08-20 13:55:12 +0900

    fix: avoid sql injection

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,17 +14,17 @@ class TaskController extends Controller
     public function getTasks(Request $request){
         $user_id = $request->user()->id;
         $SQL = <<< SQL
-        SELECT tasks.id, task_name, description, genre_name, difficulty_name, 
+        SELECT tasks.id, tasks.task_name, tasks.description, genres.genre_name, difficulties.difficulty_name, 
             CASE WHEN user_id IS NULL THEN 0 ELSE 1 END AS is_achieve 
         FROM tasks 
         LEFT JOIN ( 
-            SELECT * FROM achievements WHERE user_id = '{$user_id}'
+            SELECT * FROM achievements WHERE user_id = ?
         ) AS A ON tasks.id = A.task_id 
         JOIN genres ON genres.id = tasks.genre_id 
         JOIN difficulties ON difficulties.id = tasks.difficulty_id
         SQL;
 
-        $tasks = DB::select($SQL);
+        $tasks = DB::select($SQL, [$user_id]);
         return $tasks;
     }
 }
