896bf6058844f2743f8407be7d3e6bb7192765b7
Shunya Suzuki <syunya_0617@outlook.jp>
2021-08-23 14:51:49 +0900

    fix: refactor Lottery APIs

--- app/Http/Controllers/LotteryController.php
+++ app/Http/Controllers/LotteryController.php
@@ -2,78 +2,106 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Vote;
-use App\Models\Achievement;
 use PhpParser\Node\Expr\FuncCall;
 use Illuminate\Support\Facades\DB; 
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
-
-use function PHPUnit\Framework\isEmpty;
+use App\Models\Vote;
+use App\Models\Achievement;
+use App\Services\LotteryService;
 
 const MIN_ACHIEVEMENTS = 15;
 
 class LotteryController extends Controller
 {
+    private $lotteryService;
+
+    public function __construct(LotteryService $lotteryService ){
+        $this->lotteryService = $lotteryService;
+    }
+
     public function user_status(Request $request){
         $user = $request->user();
-        $achievements_count = $user->achievements()->count();
-        $is_voted = $user->vote()->exists();
 
-        return response()->json([
-            'is_admin' => ($user->is_admin == 1),
-            "can_vote" => ($achievements_count >= MIN_ACHIEVEMENTS && ! $is_voted),
-            "is_voted" => $is_voted
-        ], Response::HTTP_OK);
+        DB::beginTransaction();
+        try {
+            $is_admin = $this->lotteryService->is_admin($user);
+            $is_voted = $this->lotteryService->is_voted($user);
+            $can_vote = $this->lotteryService->can_vote($user);
+            DB::commit();
+
+            return response()->json([
+                'is_admin' => $is_admin,
+                "is_voted" => $is_voted,
+                "can_vote" => $can_vote
+            ], Response::HTTP_OK);
+
+        } catch (\Exception $e) {
+
+            DB::rollback();
+            return response()->json(['message' => 'Error occured'], Response::HTTP_INTERNAL_SERVER_ERROR);
+
+        }
     }
 
     public function vote(Request $request){
         $user = $request->user();
-        $user_id = $user->id;
+        $voted_number = $request->voted_number;
 
-        $achievements_count = $user->achievements()->count();
-        $is_voted = $user->vote()->exists();
-
-        if ($achievements_count < MIN_ACHIEVEMENTS){
-            return response()->json(['message' => 'Achievements is not enough'], Response::HTTP_BAD_REQUEST);
-        }
-        if ($is_voted){
-            return response()->json(['message' => 'Already voted'], Response::HTTP_BAD_REQUEST);
-        }
-        
         DB::beginTransaction();
         try {
-            $user->vote()->create(['voted_number' => $request->voted_number]);
+
+            if ($this->lotteryService->is_admin($user)){
+                DB::commit();
+                return response()->json(['message' => 'Admin can not vote'], Response::HTTP_BAD_REQUEST);
+            }
+            if ($this->lotteryService->is_voted($user)){
+                DB::commit();
+                return response()->json(['message' => 'Already voted'], Response::HTTP_BAD_REQUEST);
+            }
+            if ($this->lotteryService->achievements_count($user) < MIN_ACHIEVEMENTS){
+                DB::commit();
+                return response()->json(['message' => 'Achievements is not enough'], Response::HTTP_BAD_REQUEST);
+            }
+
+            $this->lotteryService->vote($user, $voted_number);
             DB::commit();
-
             return response()->json(['message' => 'OK'], Response::HTTP_OK);
-        } catch (\Exception $e) {
-            DB::rollback();
 
+        } catch (\Exception $e) {
+
+            DB::rollback();
             return response()->json(['message' => 'Error occured'], Response::HTTP_INTERNAL_SERVER_ERROR);
+
         }
     }
 
     public function get_winner(Request $request){
         $user = $request->user();
 
-        if ( ! $user->is_admin){
-            return response()->json(['message' => 'You are not admin'], Response::HTTP_BAD_REQUEST);
+        DB::beginTransaction();
+        try {
+            if ( !$this->lotteryService->is_admin($user)){
+                DB::commit();
+                return response()->json(['message' => 'You are not admin'], Response::HTTP_BAD_REQUEST);
+            }
+
+            $win_vote = $this->lotteryService->win_vote();
+
+            if ($win_vote === null){
+                DB::commit();
+                return response()->json(['message' => 'No one won'], Response::HTTP_OK);
+            }
+
+            $win_number = $win_vote->voted_number;
+            $winner_id = $this->lotteryService->winner_id($win_number);
+            
+            return response()->json(['user_id' => $winner_id, 'win_number' => $win_number], Response::HTTP_OK);
+
+        } catch (\Exception $e) {
+
+            DB::rollback();
+            return response()->json(['message' => 'Error occured'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        $win_vote = Vote::groupBy('voted_number')
-                ->select('voted_number', DB::raw('count(*) as voted_count'))
-                ->having('voted_count', 1)
-                ->orderBy('voted_number')
-                ->first();
-        
-        if ($win_vote === null){
-            return response()->json(['message' => 'No one won'], Response::HTTP_OK);
-        }
-
-        $win_number = $win_vote->voted_number;
-        $winner_id = Vote::where("voted_number", $win_number)->first()->user->id;
-
-        return response()->json(['user_id' => $winner_id, 'win_number' => $win_number], Response::HTTP_OK);
     }
 }
---
+++ app/Services/LotteryService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\Vote;
+
+const MIN_ACHIEVEMENTS = 15;
+
+class LotteryService{
+
+    public function is_admin($user){
+        return $user->is_admin == 1;
+    }
+
+    public function is_voted($user){
+        return $user->vote()->exists();
+    }
+
+    public function achievements_count($user){
+        return $user->achievements()->count();
+    }
+
+    public function can_vote($user){
+        return (!$this->is_admin($user) && !$this->is_voted($user) && $this->achievements_count($user) >= MIN_ACHIEVEMENTS);
+    }
+
+    public function vote($user, $voted_number){
+        $user->vote()->create(['voted_number' => $voted_number]);
+    }
+
+    public function win_vote(){
+        $win_vote = Vote::groupBy('voted_number')
+                ->select('voted_number', DB::raw('count(*) as voted_count'))
+                ->having('voted_count', 1)
+                ->orderBy('voted_number')
+                ->first();
+
+        return $win_vote;
+    }
+
+    public function winner_id($win_number){
+        return Vote::where("voted_number", $win_number)->first()->user->id;
+    }
+
+}
